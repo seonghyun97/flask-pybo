271b1befac4bdaefc006a9f50f6a6c730d8755d8
shHong <ddiamond97@naver.com>
2021-01-25 16:37:33 +0900

    dbupgrade34

--- config/production.py
+++ config/production.py
@@ -29,8 +29,8 @@ dictConfig({
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     user='dbmasteruser',
-    pw=' Wv~?x8s$B>+$0>3fg8T|ux:lbk}c&DH7 ',
-    url=' ls-60bd725f04b3f1f2544bb1b1cf438502ca63374a.cizmmhwwxovg.ap-northeast-2.rds.amazonaws.com',
+    pw=' 12345678 ',
+    url=' ls-60bd725f04b3f1f2544bb1b1cf438502ca63374a.cizmmhwwxovg.ap-northeast-2.rds.amazonaws.com ',
     db='flask_pybo'
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
